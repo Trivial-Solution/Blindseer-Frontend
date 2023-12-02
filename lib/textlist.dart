426a70f86bb01edf76d6c52ded71de32debb24ca
MarcoLoDico <marcolodico1@gmail.com>
2023-12-01 22:53:18 -0500

    Implemented live tts

--- lib/textlist.dart
+++ lib/textlist.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'services/tts.dart'; // Import the TTS class
 
 class TextListPage extends StatefulWidget {
   const TextListPage({super.key});
@@ -10,12 +10,26 @@ class TextListPage extends StatefulWidget {
 }
 
 class _TextListPageState extends State<TextListPage> {
-  FlutterTts flutterTts = FlutterTts(); // References FlutterTts
+  final TTS tts = TTS(); // Create an instance of TTS
+  String lastSpokenText = ''; // To keep track of the last spoken text
 
-  void _speak() async {
-    await flutterTts.setLanguage("en-US");
-    await flutterTts.setPitch(1); // 0.5 to 1.5 (deepness of voice)
-    await flutterTts.speak("Hello this function works");
+  @override
+  void initState() {
+    super.initState();
+    // Listen to Firestore stream
+    FirebaseFirestore.instance.collection('texts').snapshots().listen((snapshot) {
+      // Get the latest text
+      var latestText = snapshot.docs.last.data()['descr'];
+      // Check if the latest text is different from the last spoken text
+      if (latestText != lastSpokenText) {
+        _speak(latestText);
+        lastSpokenText = latestText; // Update the last spoken text
+      }
+    });
+  }
+
+  void _speak(String text) async {
+    await tts.performTextToSpeech(text); // Use the performTextToSpeech method
   }
 
   @override
@@ -34,7 +48,7 @@ class _TextListPageState extends State<TextListPage> {
         return ListView(
           children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data =
-                document.data()! as Map<String, dynamic>;
+            document.data()! as Map<String, dynamic>;
             return ListTile(
               title: Text(data['descr']),
             );
